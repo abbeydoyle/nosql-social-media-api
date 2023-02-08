51b7d3cacb61842f1e78e1afe615d3e317d49401
Abigail Doyle <abigaildoyle3@gmail.com>
2023-02-08 11:17:10 -0800

    reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,38 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+// not technically a model, only a schema
+
+const reactionSchema = new Schema(
+      {
+            reactionId: {
+                  type: Schema.Types.ObjectId,
+                  default: () => new Types.ObjectId(),
+            },
+
+            reactionBody: {
+                  type: String,
+                  required: true,
+                  maxlength: 50,
+            },
+
+            username: {
+                  type: String,
+                  required: true,
+            },
+
+            createdAt: {
+                  type: Date,
+                  default: Date.now,
+            }
+      },
+
+      {
+            toJSON: {
+                  getters: true,
+            },
+
+            id: false
+      }
+)
+
+module.exports = reactionSchema;
